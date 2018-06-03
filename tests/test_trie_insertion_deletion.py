3d3b9b60644432dbc1e9e7537220c5d01e73179c
Lovesh Harchandani <lovesh.bond@gmail.com>
2018-06-03 16:00:32 -0600

    KeyError if key is not present
    Signed-off-by: Lovesh Harchandani <lovesh.bond@gmail.com>

--- tests/test_trie_insertion_deletion.py
+++ tests/test_trie_insertion_deletion.py
@@ -1,6 +1,8 @@
 from copy import deepcopy
 from random import randint
 
+import pytest
+
 from tests.helper import random_string
 
 
@@ -15,7 +17,7 @@ def add_and_check_key_vals_to_trie(trie, key_vals):
         assert v == trie.get(k)
         v, proof = trie.get(k, with_proof=True)
         proof.append(root_node)
-        assert trie.verify_proof(root_hash, k, v, proof_nodes=proof)
+        assert trie.verify_proof_of_existence(root_hash, k, v, proof_nodes=proof)
 
 
 def test_update_get_from_trie_small(ephem_trie):
@@ -62,15 +64,83 @@ def test_update_get_different_roots(ephem_trie):
         assert b'v3' == trie.get(key)
         _, proof = trie.get(key, with_proof=True)
         proof.append(deepcopy(trie.root_node))
-        assert trie.verify_proof(trie.root_hash, key, b'v3', proof_nodes=proof)
+        assert trie.verify_proof_of_existence(trie.root_hash, key, b'v3', proof_nodes=proof)
 
         assert b'v2' == trie.get(key, root_node=old_root2)
         assert b'v1' == trie.get(key, root_node=old_root1)
 
         _, proof = trie.get(key, root_node=old_root2, with_proof=True)
         proof.append(old_root2)
-        assert trie.verify_proof(old_root_hash2, key, b'v2', proof_nodes=proof)
+        assert trie.verify_proof_of_existence(old_root_hash2, key, b'v2', proof_nodes=proof)
 
         _, proof = trie.get(key, root_node=old_root1, with_proof=True)
         proof.append(old_root1)
-        assert trie.verify_proof(old_root_hash1, key, b'v1', proof_nodes=proof)
+        assert trie.verify_proof_of_existence(old_root_hash1, key, b'v1', proof_nodes=proof)
+
+
+def test_non_existing_keys(ephem_trie):
+    trie = ephem_trie
+    trie.update(b'k1', b'v1')
+    trie.update(b'k2', b'v2')
+    trie.update(b'x3', b'v3')
+    trie.update(b'y4', b'v4')
+    trie.update(b'z3', b'v5')
+
+    assert b'v1' == trie.get(b'k1')
+    with pytest.raises(KeyError) as err:
+        trie.get(b'k')
+
+    old_root1 = deepcopy(trie.root_node)
+
+    trie.update(b'k', b'v')
+    assert b'v' == trie.get(b'k')
+
+    with pytest.raises(KeyError) as err:
+        trie.get(b'k', root_node=old_root1)
+
+    with pytest.raises(KeyError) as err:
+        trie.get(b'43')
+
+    with pytest.raises(KeyError) as err:
+        trie.get(b'pq1')
+
+    with pytest.raises(KeyError) as err:
+        trie.get(b'k11')
+
+    with pytest.raises(KeyError) as err:
+        trie.get(b'y44')
+
+    trie.update(b'abcd1', b'x1')
+    trie.update(b'abcd2', b'x2')
+    trie.update(b'abcd3', b'x3')
+    trie.update(b'abcd11', b'x4')
+    trie.update(b'abcd12', b'x4')
+    trie.update(b'abcd21', b'x5')
+    trie.update(b'abcd1111', b'x6')
+    trie.update(b'abcd11112', b'x7')
+
+    with pytest.raises(KeyError) as err:
+        trie.get(b'abcd')
+    with pytest.raises(KeyError) as err:
+        trie.get(b'abcd111')
+
+
+def test_non_existing_keys_large(ephem_trie):
+    trie = ephem_trie
+    key_vals = {}
+    for _ in range(10000):
+        key_vals[random_string(randint(30, 50)).encode()] = random_string(
+            randint(100, 1000)).encode()
+
+    add_and_check_key_vals_to_trie(trie, key_vals)
+
+    # Choose 1000 non existent keys
+    non_existent_keys = set()
+    while len(non_existent_keys) < 1000:
+        k = random_string(randint(30, 50)).encode()
+        if k not in key_vals:
+            non_existent_keys.add(k)
+
+    for k in non_existent_keys:
+        with pytest.raises(KeyError) as err:
+            trie.get(k)
